7744a68e6b5d98a4c26751acfe29998b7174bc95
Derek Lane <wahoskendev@gmail.com>
2026-05-03 21:13:57 -0400

    increased button size. added fullscreen button

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,13 +1,24 @@
 extends Control
 
+@onready var fullscreen_button: TextureButton = $FullscreenButton
 
 func _ready() -> void:
 	hide()
 
+	if fullscreen_button:
+		fullscreen_button.pressed.connect(_on_fullscreen_button_pressed)
 
-func _input(event):
+func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
 		show()
 
 	if event is InputEventKey:
 		hide()
+
+func _on_fullscreen_button_pressed() -> void:
+	var current_mode := DisplayServer.window_get_mode()
+
+	if current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
